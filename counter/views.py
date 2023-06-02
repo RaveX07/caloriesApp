edeeee537c313b3706efa9b123ca6cb43b13d104
RaveX07 <jannis.schroeter@gmx.de>
2023-06-02 16:38:16 +0200

    restructure project

--- counter/views.py
+++ counter/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 import requests
 import json
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         query = request.POST['query']
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-        api_response = requests.get(api_url, headers={'X-Api-Key': 'g0JEDfQ6PODMiVqjvAhQPA==gWc0vuMdQQIEYy3e'})
+        api_response = requests.get(api_url, headers={'X-Api-Key': os.getenv('API_KEY')})
 
         try: 
             api = json.loads(api_response.content)
